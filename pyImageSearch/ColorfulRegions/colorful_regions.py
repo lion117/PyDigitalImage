12785a930795f1da4078910d7c0b11b1d163dcb4
hesiyuan <hesiyuan117@qq.com>
2018-06-27 18:28:08 +0800

    new two interst project

---
+++ pyImageSearch/ColorfulRegions/colorful_regions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import sys, os, time
+# https://www.pyimagesearch.com/2017/06/26/labeling-superpixel-colorfulness-opencv-python/
+
+
+
+
+if __name__ == "__main__":
+    print
+    os.getcwd()
---
+++ pyImageSearch/DistanceToCamera/distance_to_camera.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import sys, os, time
+# https://www.pyimagesearch.com/2015/01/19/find-distance-camera-objectmarker-using-python-opencv/
+
+if __name__ == "__main__":
+    print
+    os.getcwd()
