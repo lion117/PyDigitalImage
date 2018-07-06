1ef31bda2f68a3d370394130f632474426e0ee07
hesiyuan <hesiyuan117@qq.com>
2018-07-06 08:52:34 +0800

    add colorful project

--- pyImageSearch/ColorfulRegions/colorful_regions.py
+++ pyImageSearch/ColorfulRegions/colorful_regions.py
@@ -34,59 +34,67 @@ def segment_colorfulness(image, mask):
 	return stdRoot + (0.3 * meanRoot)
 
 
+def ExtractSuperPixImage(tImage , tSegments = 100):
+	# construct the argument parse and parse the arguments
+	# ap = argparse.ArgumentParser()
+	# ap.add_argument("-i", "--image", required=True,
+	# 				help="path to input image")
+	# ap.add_argument("-s", "--segments", type=int, default=100,
+	# 				help="# of superpixels")
+	# args = vars(ap.parse_args())
+	if os.path.exists(tImage) is False:
+		print(u"%s file is not exist"%(tImage))
+		return
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-ap.add_argument("-s", "--segments", type=int, default=100,
-	help="# of superpixels")
-args = vars(ap.parse_args())
+	# load the image in OpenCV format so we can draw on it later, then
+	# allocate memory for the superpixel colorfulness visualization
+	orig = cv2.imread(tImage)
+	vis = np.zeros(orig.shape[:2], dtype="float")
+	# load the image and apply SLIC superpixel segmentation to it via
+	# scikit-image
+	image = io.imread(tImage)
+	segments = slic(img_as_float(image), n_segments=tSegments,
+					slic_zero=True)
+	# loop over each of the unique superpixels
+	for v in np.unique(segments):
+		# construct a mask for the segment so we can compute image
+		# statistics for *only* the masked region
+		mask = np.ones(image.shape[:2])
+		mask[segments == v] = 0
+
+		# compute the superpixel colorfulness, then update the
+		# visualization array
+		C = segment_colorfulness(orig, mask)
+		vis[segments == v] = C
+
+	# scale the visualization image from an unrestricted floating point
+	# to unsigned 8-bit integer array so we can use it with OpenCV and
+	# display it to our screen
+	vis = rescale_intensity(vis, out_range=(0, 255)).astype("uint8")
+	# overlay the superpixel colorfulness visualization on the original
+	# image
+	alpha = 0.6
+	overlay = np.dstack([vis] * 3)
+	output = orig.copy()
+	cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+	# show the output images
+	cv2.imshow("Input", orig)
+	cv2.imshow("Visualization", vis)
+	cv2.imshow("Output", output)
+	cv2.waitKey(0)
 
 
-# load the image in OpenCV format so we can draw on it later, then
-# allocate memory for the superpixel colorfulness visualization
-orig = cv2.imread(args["image"])
-vis = np.zeros(orig.shape[:2], dtype="float")
 
-# load the image and apply SLIC superpixel segmentation to it via
-# scikit-image
-image = io.imread(args["image"])
-segments = slic(img_as_float(image), n_segments=args["segments"],
-	slic_zero=True)
+class MainTest():
+	@staticmethod
+	def testExtractSuperPixImage():
+		lImage = u"example1.png"
+		lImage2 = u"example3.jpg"
+		lSegement = 200
+		ExtractSuperPixImage(lImage2,lSegement)
 
-# loop over each of the unique superpixels
-for v in np.unique(segments):
-	# construct a mask for the segment so we can compute image
-	# statistics for *only* the masked region
-	mask = np.ones(image.shape[:2])
-	mask[segments == v] = 0
-
-	# compute the superpixel colorfulness, then update the
-	# visualization array
-	C = segment_colorfulness(orig, mask)
-	vis[segments == v] = C
-
-
-# scale the visualization image from an unrestricted floating point
-# to unsigned 8-bit integer array so we can use it with OpenCV and
-# display it to our screen
-vis = rescale_intensity(vis, out_range=(0, 255)).astype("uint8")
-
-# overlay the superpixel colorfulness visualization on the original
-# image
-alpha = 0.6
-overlay = np.dstack([vis] * 3)
-output = orig.copy()
-cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
-
-# show the output images
-cv2.imshow("Input", orig)
-cv2.imshow("Visualization", vis)
-cv2.imshow("Output", output)
-cv2.waitKey(0)
 
 
 if __name__ == "__main__":
-    print
-    os.getcwd()
+	print (os.getcwd())
+	MainTest.testExtractSuperPixImage()
